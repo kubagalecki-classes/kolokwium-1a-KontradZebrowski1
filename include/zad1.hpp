8681660e10330df0dff4276f91b85e3dcdb453c3
KontradZebrowsk <konzebr@gmail.com>
2021-01-14 11:35:37 +0000

    Pierwsza proba zad 1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,3 +6,23 @@
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
+class Tagliatelle
+{
+public:
+  Tagliatelle(double l, double w, double r): L(l), W(w), R(r) {};
+  Tagliatelle(): L(0.5), W(0.5), R(0.5) {};
+
+  
+private:
+  double        L=0;
+  double        W=0;
+  double        R=0;
+  const double  C;
+
+public:
+  
+  double ileMaki(unsigned P) const
+  {
+    return P * L * W * (1. - R) * C;
+  }
+};
